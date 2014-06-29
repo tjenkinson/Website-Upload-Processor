2fc5cc0aa20ad8838e703bfcb8e99f3a31d5957e
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-06-29 13:52:10 +0100

    Basic hello world
    Created using maven

---
+++ src/main/java/uk/co/la1tv/websiteUploadProcessor/App.java
@@ -0,0 +1,13 @@
+package uk.co.la1tv.websiteUploadProcessor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
