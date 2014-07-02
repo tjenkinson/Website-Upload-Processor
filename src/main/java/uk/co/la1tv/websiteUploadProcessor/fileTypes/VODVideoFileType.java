3ff4e95632c37998a3c801723abf7b26a2629b73
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-07-02 09:49:13 +0100

    now loads ffmpeg and gets exit code

--- src/main/java/uk/co/la1tv/websiteUploadProcessor/fileTypes/VODVideoFileType.java
+++ src/main/java/uk/co/la1tv/websiteUploadProcessor/fileTypes/VODVideoFileType.java
@@ -1,8 +1,13 @@
 package uk.co.la1tv.websiteUploadProcessor.fileTypes;
 
+import java.io.IOException;
+
 import org.apache.log4j.Logger;
 
+import uk.co.la1tv.websiteUploadProcessor.Config;
 import uk.co.la1tv.websiteUploadProcessor.File;
+import uk.co.la1tv.websiteUploadProcessor.helpers.StreamGobbler;
+import uk.co.la1tv.websiteUploadProcessor.helpers.StreamType;
 
 public class VODVideoFileType extends FileTypeAbstract {
 	
@@ -14,8 +19,32 @@ public class VODVideoFileType extends FileTypeAbstract {
 
 	@Override
 	public boolean process(java.io.File source, java.io.File workingDir, File file) {
-		// TODO Auto-generated method stub
-		return false;
+		Config config = Config.getInstance();
+		
+		logger.debug("Starting ffmpeg...");
+		int exitVal;
+		try {
+			Runtime rt = Runtime.getRuntime();
+			Process proc = rt.exec(config.getString("ffmpeg.location"), null, workingDir);
+			// consume the err stream and stdout stream from the process
+			new StreamGobbler(proc.getErrorStream(), StreamType.ERR).start();
+			new StreamGobbler(proc.getInputStream(), StreamType.STDOUT).start(); // the input stream is the STDOUT from the program being executed
+			exitVal = proc.waitFor();
+		
+		} catch (IOException e) {
+			throw(new RuntimeException("Error trying to execute ffmpeg."));
+		} catch (InterruptedException e) {
+			throw(new RuntimeException("InterruptException occured. This shouldn't happen."));
+		}
+		
+		if (exitVal == 0) {
+			logger.debug("ffmpeg finished successfully with error code "+exitVal+".");
+		}
+		else {
+			logger.warn("ffmpeg finished but returned error code "+exitVal+".");
+		}
+		
+		return exitVal == 0;
 	}
 
 }
--- src/main/java/uk/co/la1tv/websiteUploadProcessor/helpers/StreamType.java
+++ src/main/java/uk/co/la1tv/websiteUploadProcessor/helpers/StreamType.java
@@ -1,5 +1,6 @@
 package uk.co.la1tv.websiteUploadProcessor.helpers;
 
 public enum StreamType {
-	STD, ERR
+	STDOUT,
+	ERR
 }
