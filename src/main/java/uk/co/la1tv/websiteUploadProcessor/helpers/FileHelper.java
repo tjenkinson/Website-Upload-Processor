f236b033117a994d35c5ad9c346dd08ca392aa7a
Tom Jenkinson <tjcode@outlook.com>
2014-09-26 12:51:06 +0100

    debugging

--- src/main/java/uk/co/la1tv/websiteUploadProcessor/helpers/FileHelper.java
+++ src/main/java/uk/co/la1tv/websiteUploadProcessor/helpers/FileHelper.java
@@ -91,7 +91,8 @@ public class FileHelper {
 	}
 	
 	public static boolean isOverQuota(BigInteger additional) {
-		Config config = Config.getInstance();		
+		Config config = Config.getInstance();
+		logger.debug("DEBUGGING: "+config.getBigInteger("general.webAppSpaceQuota").multiply(new BigInteger("1000000"))+" ------- "+FileUtils.sizeOfAsBigInteger(new File(FileHelper.format(config.getString("files.webappFilesLocation")))));
 		return FileUtils.sizeOfAsBigInteger(new File(FileHelper.format(config.getString("files.webappFilesLocation")))).compareTo(config.getBigInteger("general.webAppSpaceQuota").multiply(new BigInteger("1000000")).add(additional)) > 0 ;
 	}
 }
