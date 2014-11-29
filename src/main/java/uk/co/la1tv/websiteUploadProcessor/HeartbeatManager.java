076453911812c094d43f69bbbfaa2a1bdd752901
Tom Jenkinson <tjcode@outlook.com>
2014-11-29 21:10:04 +0000

    updated heartbeat manager so it should not be possible for 2 instances to pick up the same job

--- src/main/java/uk/co/la1tv/websiteUploadProcessor/HeartbeatManager.java
+++ src/main/java/uk/co/la1tv/websiteUploadProcessor/HeartbeatManager.java
@@ -67,11 +67,15 @@ public class HeartbeatManager {
 			
 			logger.info("Registering file with id "+file.getId()+" with HeartbeatManager...");
 			// get a new connection to the database. Important because transactions are used and other java threads should not end up using the same connection.
+			
+			
 			Connection dbConnection = DbHelper.getMainDb().getConnection();
 			try {
 				dbConnection.prepareStatement("START TRANSACTION").executeUpdate();
+				// getting an exclusive lock is important as it makes sure we are reading the latest timestamp
 				PreparedStatement s = dbConnection.prepareStatement("SELECT heartbeat FROM files WHERE id=? FOR UPDATE");
 				s.setInt(1, file.getId());
+				long timeRequestMade = System.currentTimeMillis();
 				ResultSet r = s.executeQuery();
 				if (!r.next()) {
 					logger.debug("Error trying to register file with id "+file.getId()+". It could not be found. Could have just been deleted.");
@@ -81,10 +85,25 @@ public class HeartbeatManager {
 					return false;
 				}
 				
-				// check that that the heartbeat hasn't been updates somewhere else
+				long timeTakenToGetResponse = System.currentTimeMillis() - timeRequestMade;
+				
 				Timestamp lastHeartbeat = r.getTimestamp("heartbeat");
+				
+				// have to presume whilst this was waiting for the lock to read the timestamp, the server with is currently processing the file was unable to update the time, because it is also waiting for a lock.
+				// e.g.
+				// server 1 already has registered a file
+				// server 1 get exclusive lock on file record and updates the timestamp on the file record at time 10
+				// server 2 makes query to get the current timestamp from the file record at time 11
+				// for whatever reason the file record is currently locked so server 2 is still waiting to get the time
+				// server 1 makes query to get lock on the file record, so that it can then update it, at time 40. It can't get a lock at the moment as it is already locked somewhere else
+				// the file record becomes unlocked, so then server 2 gets the lock as it was next in line, and gets the result, at time 45. The result it would get is 10
+				// server 2 now releases the lock and server 1 gets it and writes in the current time at time 46
+				
+				// therefore adding the time taken to get the response to the time that is received works
+				long lastHeartbeatTime = lastHeartbeat.getTime() + timeTakenToGetResponse;
 				s.close();
-				if (lastHeartbeat != null && lastHeartbeat.getTime() >= getProcessingFilesTimestamp().getTime()) {
+				// check that that the heartbeat hasn't been updates somewhere else
+				if (lastHeartbeat != null && lastHeartbeatTime >= getProcessingFilesTimestamp().getTime()) {
 					logger.debug("Could not register file with id "+file.getId()+" because it appears that it has been updated somewhere else.");
 					dbConnection.prepareStatement("COMMIT").executeUpdate();
 					dbConnection.close();
@@ -112,6 +131,9 @@ public class HeartbeatManager {
 				} catch (SQLException e1) {
 					logger.debug("Transaction failed to be rolled back. This is possible if the reason is that the transaction failed to start in the first place.");
 				}
+				
+				// TODO: the exception could be due to a lock timeout. need to kill any threads that think they have the lock
+				
 				throw(new RuntimeException("Error trying to register a file with HeartbeatManager."));
 			}		
 
@@ -153,7 +175,7 @@ public class HeartbeatManager {
 	// returns the Timestamp that should be used to determine if a file is being processed
 	// files with timestamps less than this value are no longer being processed
 	public Timestamp getProcessingFilesTimestamp() {
-		return new Timestamp(System.currentTimeMillis() - updateInterval - 5000);
+		return new Timestamp(System.currentTimeMillis() - updateInterval - 30000);
 	}
 	
 	private class Task extends TimerTask {
@@ -184,21 +206,42 @@ public class HeartbeatManager {
 				fileIdsWhere += ")";
 				
 				try {
-					PreparedStatement s = dbConnection.prepareStatement("UPDATE files SET heartbeat=? WHERE id IN "+fileIdsWhere);
-					Timestamp currentTimestamp = new Timestamp(System.currentTimeMillis());
+					dbConnection.prepareStatement("START TRANSACTION").executeUpdate();
+					// first get an exclusive lock on all the records that the timestamps need updating on
+					PreparedStatement s = dbConnection.prepareStatement("SELECT heartbeat FROM files WHERE id IN "+fileIdsWhere+" FOR UPDATE");
 					int paramNo = 1;
+					for (FileAndCounter fileAndCounter : files) {
+						File file = fileAndCounter.getFile();
+						s.setInt(paramNo++, file.getId());
+					}
+					s.executeQuery();
+					
+					// now that we have an exclusive lock we can be confident that this query will execute pretty instantly and therefore the time will be accurate.
+					// if we didn't get the lock above then this update command would need to get an exclusive lock, which could take some time, meaning then when it gets the lock the time that would be written would be old
+					// whenever a server tries to register a file they first request an exclusive lock on the record.
+					// provided that all requests to the mysql server with exclusive locks are handled in the order that the locks were requested, there should be no issues
+					Timestamp currentTimestamp = new Timestamp(System.currentTimeMillis());
+					s = dbConnection.prepareStatement("UPDATE files SET heartbeat=? WHERE id IN "+fileIdsWhere);
+					paramNo = 1;
 					s.setTimestamp(paramNo++, currentTimestamp);
 					for (FileAndCounter fileAndCounter : files) {
 						File file = fileAndCounter.getFile();
 						s.setInt(paramNo++, file.getId());
 					}
 					if (s.executeUpdate() != files.size()) {
-						logger.warn("Error occurred when updating heartbeat timestamps.");
+						logger.warn("Error occurred when updating heartbeat timestamps. Some may not have been updated.");
 					}
+					dbConnection.prepareStatement("COMMIT").executeUpdate();
+					
 					s.close();
 				
 				} catch (SQLException e) {
-					throw(new RuntimeException("Error trying to update files from HeartbeatManager."));
+					try {
+						dbConnection.prepareStatement("ROLLBACK").executeUpdate();
+					} catch (SQLException e1) {
+						logger.debug("Transaction for updating heartbeat timestamps failed to be rolled back. This is possible if the reason is that the transaction failed to start in the first place.");
+					}
+					throw(new RuntimeException("Error trying to update files with HeartbeatManager."));
 				}
 			}
 			logger.debug("Finished updating heartbeat timestamps.");
